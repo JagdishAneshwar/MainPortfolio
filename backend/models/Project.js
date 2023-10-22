cc0ebed8c3ac5fb8125dd871499bc39e24e2f8cf
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2023-10-23 00:50:16 +0530

    latest

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema({
@@ -14,20 +13,3 @@ const projectSchema = new mongoose.Schema({
   });
 
   module.exports = mongoose.model('project', projectSchema)
-=======
-const mongoose = require("mongoose");
-
-const projectSchema = new mongoose.Schema({
-    projectName: String,
-    projectDescription: String,
-    date: Date,
-    features: String,
-    technology: String,
-    webpageLink: String,
-    gitRepoLink: String,
-    mediumArticleLink: String,
-    projectImage: String,
-  });
-
-  module.exports = mongoose.model('project', projectSchema)
->>>>>>> 4709fd4 (Add all my files)
