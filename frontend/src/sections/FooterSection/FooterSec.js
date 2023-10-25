703615e34198e17aacf7dbdc9dbb517946a2a41e
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2023-10-25 17:01:25 +0530

    Update FooterSec.js

--- frontend/src/sections/FooterSection/FooterSec.js
+++ frontend/src/sections/FooterSection/FooterSec.js
@@ -6,7 +6,7 @@ export default function FooterSec() {
     <div className='footer-section'>
     <div className="my-email-wrapper"><div className="my-email"><span className='footer-email'>aneshwarjagdish</span>@gmail.com</div><div className="my-email">+91 <span className='footer-nummber'>8928393524</span></div>
     </div>
-    <p className='copyright'><small><span className='copyright-symbol'>&copy;</span> Copyright 2023, Jagdish Aneshwar</small></p>
+    <p className='copyright'><small><span className='copyright-symbol'>&copy;</span> Copyright 2023, Jagdish Aneshwar</small><script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script></p>
     </div>
   )
 }
