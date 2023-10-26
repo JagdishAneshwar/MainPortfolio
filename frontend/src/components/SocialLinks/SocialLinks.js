d9ca5c76fadee2851a205d6185d85b6b6eebd5fe
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2023-10-26 10:17:02 +0530

    social links are added

--- frontend/src/components/SocialLinks/SocialLinks.js
+++ frontend/src/components/SocialLinks/SocialLinks.js
@@ -11,22 +11,22 @@ export default function SocialLinks() {
 
 <div className="offcanvas social-wrapper offcanvas-start bg-dark"data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions1" aria-labelledby="offcanvasWithBothOptionsLabel">
     <div className="icon-wrapper">
-    <div class="icons8-github"></div>
+    <div class="icons8-github" href="https://github.com/JagdishAneshwar"></div>
     </div>
     <div className="icon-wrapper">
-    <div class="icons8-whatsapp"></div>
+    <div class="icons8-whatsapp" href="https://wa.link/91sbjb"></div>
     </div>
     <div className="icon-wrapper">
-    <div class="icons8-linkedin"></div>
+    <div class="icons8-linkedin" href="https://www.linkedin.com/in/jagdish-aneshwar/"></div>
     </div>
     <div className="icon-wrapper">
-    <div class="icons8-facebook"></div>
+    <div class="icons8-facebook" href="https://www.facebook.com/JagdishAneshwar"></div>
     </div>
     <div className="icon-wrapper">
-    <div class="icons8-instagram"></div>
+    <div class="icons8-instagram" href="https://www.instagram.com/jagdishaneshwar/"></div>
     </div>
     <div className="icon-wrapper">
-    <div class="icons8-medium"></div>
+    <div class="icons8-medium" href="https://medium.com/@jagdish_aneshwar"></div>
     </div>
 </div></>
   )
