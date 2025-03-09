f88e42648e199f2c109ca26aa45984f7a769558d
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2025-03-10 01:30:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://www.thejagdish.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
